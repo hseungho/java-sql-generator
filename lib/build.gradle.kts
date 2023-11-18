26468ed6070c0f24e90468c43b26b661387aca9a
hseungho <hsh47607@gmail.com>
2023-11-18 20:33:27 +0900

    build: jdk version 명시
    Signed-off-by: hseungho <hsh47607@gmail.com>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,6 +10,10 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
