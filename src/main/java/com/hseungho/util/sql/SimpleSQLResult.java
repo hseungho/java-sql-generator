cea4e74e2e26303f0eeffa74c15b8a2c63d05dba
hseungho <hsh47607@gmail.com>
2023-11-20 11:54:11 +0900

    feat: Add SQLResult createFile function
    Signed-off-by: hseungho <hsh47607@gmail.com>

--- src/main/java/com/hseungho/util/sql/SimpleSQLResult.java
+++ src/main/java/com/hseungho/util/sql/SimpleSQLResult.java
@@ -1,5 +1,8 @@
 package com.hseungho.util.sql;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
@@ -27,6 +30,23 @@ class SimpleSQLResult implements SQLResult {
         StringJoiner sj = new StringJoiner("\n");
         this.results.forEach(sj::add);
         System.out.println(sj);
+    }
+
+    @Override
+    public boolean createFile(String path) {
+        try {
+            File file = new File(path);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file, false), 8 * 1024);
+
+            String result = String.join("\n", this.results);
+            bw.append(result);
+            bw.flush();
+            bw.close();
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
